0cdb4b167e2245856840222fc02b6609f77f534c
Pearl-Pko <osamuede75@gmail.com>
2024-05-21 14:30:19 +0100

    GOD ABEG

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
-    password: process.env.PGPASSOWRD,
+    password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
 });
 
