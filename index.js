7ea2d6f501bb2e973799ff73a777af2c6c54ecf1
Pearl-Pko <osamuede75@gmail.com>
2024-06-27 21:38:43 +0100

    Added security headers to api endpoints

--- index.js
+++ index.js
@@ -4,33 +4,39 @@ const session = require("express-session");
 const app = express();
 const logger = require("morgan");
 const cors = require("cors");
-const pgSession = require('connect-pg-simple')(session);
+const pgSession = require("connect-pg-simple")(session);
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 const pool = require("./db"); // Import the PostgreSQL connection pool
-
 
 const port = 5000;
 
 app.use(logger("combined"));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
+app.use(helmet());
+
 
 app.use(
     session({
         store: new pgSession({
-            
             pool: pool, // Connection pool
             tableName: "session", // Use another table-name than the default "session" one
             // Insert connect-pg-simple options here
         }),
         secret: process.env.SECRET, // Session secret for encryption
         resave: false,
-        
+
         saveUninitialized: true,
 
-        cookie: {secure: false, maxAge: 3.1536e10}, // Cookie settings (e.g., secure: true for HTTPS)
+        cookie: {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 3.1536e10,
+        }, 
     })
 );
 
@@ -40,7 +46,7 @@ const authRoutes = require("./routes/auth");
 // Import routes
 const userRoutes = require("./routes/user");
 
-const adminRoutes = require("./routes/admin")
+const adminRoutes = require("./routes/admin");
 
 // Mount routes
 app.use("/", authRoutes);
@@ -49,7 +55,7 @@ app.use("/lecturer", adminRoutes);
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: "something went wrong"});
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
